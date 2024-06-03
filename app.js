c82c7dc2db277a1480bf8b3527713793b2ef5842
joneprone <aquatrizor@gmail.com>
2024-06-03 18:56:11 -0400

    Added UI and assets for the UI

--- app.js
+++ app.js
@@ -3,87 +3,109 @@ let humanChoice = undefined;
 let computerChoice = undefined;
 let humanScore = 0;
 let computerScore = 0;
-let currentHigh = 0;
 
+const options = document.querySelectorAll(".choose")
+const htmlHumanChoice = document.querySelector("#humanChoice");
+const htmlHumanScore = document.querySelector("#humanScore");
+const htmlComputerScore = document.querySelector("#computerScore");
+const htmlComputerChoice = document.querySelector("#computerChoice");
+const modal = document.querySelector("#simpleModal");
+const winner = document.querySelector("#winner");
+const playAgainBtn  = document.querySelector("#playAgain")
 
-const getHumanChoice = () => {
-humanChoice = prompt("Rock, Paper, Scissors!");
-humanChoice = humanChoice.toLowerCase();
-return humanChoice;
-}
+options.forEach(function(element, humanChoice) {
 
-const getComputerChoice = () => {
-let temp = Math.floor(Math.random()*3);
+    element.addEventListener("click", function() {
 
-if (temp == 0) {
-    computerChoice = "rock";
-} 
-else if (temp == 1){
-    computerChoice = "paper";
-} 
-else if (temp == 2){
-    computerChoice = "scissors";
-}
-return computerChoice;
-}
-
-
-
-const playRound = () => {
-    getHumanChoice();
-    getComputerChoice();
-    if (humanChoice == computerChoice){
-        alert(`TIe! Computer: ${computerScore} You: ${humanScore}`)
-    } if (humanChoice == "rock" && computerChoice == "scissors") {
-        humanScore++;
-        alert(`You won that round! Computer: ${computerScore} You: ${humanScore}`);
-    } else if (humanChoice == "paper" && computerChoice == "rock") {
-        humanScore++;
-        alert(`You won that round! Computer: ${computerScore} You: ${humanScore}`);
-    } else if (humanChoice == "scissors" && computerChoice == "paper") {
-        humanScore++;
-        alert(`You won that round! Computer: ${computerScore} You: ${humanScore}`);
-    } else if (humanChoice == "rock" && computerChoice == "paper"){
-        computerScore++;
-        alert(`Computer won that round! Computer: ${computerScore} You: ${humanScore}`);
-    } else if (humanChoice == "paper" && computerChoice == "scissors"){
-        computerScore++;
-        alert(`Computer won that round! Computer: ${computerScore} You: ${humanScore}`);
-    } else if (humanChoice == "scissors" && humanChoice == "rock") {
-        computerScore++;
-        alert(`Computer won that round! Computer: ${computerScore} You: ${humanScore}`);
-    } else {
-        alert(`maybe a spelling mistake? Or empty? ==> [${humanChoice}]`)
-    }
+        let temp = Math.floor(Math.random()*3);
+                if (temp == 0) {
+            computerChoice = "rock";
     
-    if (humanScore > computerScore){
-        currentHigh = humanScore;
-    } else if (computerScore > humanScore){
-        currentHigh = computerScore;
-    } else if (humanScore == computerScore) {
-        currentHigh = computerScore;
-    }
-    return currentHigh;
-}
+        } 
+        else if (temp == 1){
+            computerChoice = "paper";
+        } 
+        else if (temp == 2){
+            computerChoice = "scissors";
+        } 
+        console.log(computerChoice)
+        
 
-const playGame = () => {
-let playAgain = true;
-
-    while(playAgain == true){
-        while (currentHigh < 3){
-            playRound();
+        if (element.id == "rock"){
+            humanChoice = element.id;
         }
-        if (currentHigh == humanScore) {
-        playAgain = confirm(`Congrats! You won! Would you like to play Again?`);
-        playAgain;
-        humanScore, computerScore, currentHigh = 0;
-        } else if (currentHigh == computerScore) {
-        playAgain = confirm(`The computer won! Would you like to play Again?`);
-        playAgain;
-        humanScore, computerScore, currentHigh = 0;
+        else if (element.id == "paper"){
+            humanChoice = element.id;
+        } 
+        else if (element.id == "scissors") {
+            humanChoice = element.id;
         }
-    }
+        console.log(humanChoice)
 
-}
 
-playGame();
+                if (humanChoice == computerChoice){
+                    htmlHumanChoice.style.color="black"
+                    htmlComputerChoice.style.color="black"
+
+                } if (humanChoice == "rock" && computerChoice == "scissors") {
+                    humanScore++;
+                    htmlHumanChoice.style.color="green"
+                    htmlComputerChoice.style.color="red"
+
+                } else if (humanChoice == "paper" && computerChoice == "rock") {
+                    humanScore++;
+                    htmlHumanChoice.style.color="green"
+                    htmlComputerChoice.style.color="red"
+
+                } else if (humanChoice == "scissors" && computerChoice == "paper") {
+                    humanScore++;
+                    htmlHumanChoice.style.color="green"
+                    htmlComputerChoice.style.color="red"
+
+                } else if (humanChoice == "rock" && computerChoice == "paper"){
+                    computerScore++;
+                    htmlHumanChoice.style.color="red"
+                    htmlComputerChoice.style.color="green"
+
+                } else if (humanChoice == "paper" && computerChoice == "scissors"){
+                    computerScore++;
+                    htmlHumanChoice.style.color="red"
+                    htmlComputerChoice.style.color="green"
+
+                } else if (humanChoice == "scissors" && humanChoice == "rock") {
+                    computerScore++;
+                    htmlHumanChoice.style.color="red"
+                    htmlComputerChoice.style.color="green"
+                }
+
+            htmlHumanChoice.textContent = `You chose ${humanChoice}`
+            htmlComputerChoice.textContent = `Computer chose ${computerChoice}`
+            htmlHumanScore.textContent = `Your score: ${humanScore}`
+            htmlComputerScore.textContent = `Computer score: ${computerScore}`
+
+        if (computerScore == 5) {
+            modal.style.display = 'block';
+            winner.textContent = "Computer Wins!"
+
+        }  
+        else if (humanScore == 5){
+            modal.style.display = 'block';
+            winner.textContent = "You Win!"
+
+        }
+    });
+
+});
+
+playAgainBtn.addEventListener("click", function() {
+    modal.style.display = 'none';
+    humanScore = 0
+    computerScore = 0
+    htmlHumanScore.textContent = `Your score: ${humanScore}`
+    htmlComputerScore.textContent = `Computer score: ${computerScore}`
+    htmlHumanChoice.textContent = `You chose `
+    htmlComputerChoice.textContent = `Computer chose `
+    htmlHumanChoice.style.color="black"
+    htmlComputerChoice.style.color="black"
+});
+
